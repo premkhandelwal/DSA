db8b59bb1b9aba17f8a086c8f3d3cdc48eb47ee1
Prem Khandelwal <prem.dkhandelwal@gmail.com>
2022-08-08 17:11:05 +0530

    Update romanToInt.cpp

--- romanToInt.cpp
+++ romanToInt.cpp
@@ -37,3 +37,23 @@ public:
     return num;
     }
 };
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------
+    Approach2
+    
+    class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> romanChar = {{'M', 1000}, {'D', 500}, {'C', 100}, {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1}};
+        int num = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(romanChar[s[i]] < romanChar[s[i + 1]]){
+                num -= romanChar[s[i]];
+            }else{
+                num += romanChar[s[i]]; 
+            }
+        }
+        return num;
+    }
+};
