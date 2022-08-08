0fadc07f3139ffd5f48051c63b44cbe5ff8280a9
Prem Khandelwal <prem.dkhandelwal@gmail.com>
2022-08-08 17:15:03 +0530

    Create twoSum.cpp

---
+++ twoSum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+        for(int i = 0; i< nums.size(); i++){
+            int comp = target - nums[i];
+            if(m.count(comp)) return {i,m[comp]};
+            m[nums[i]] = i;
+        }
+        return {};
+    }
+};
