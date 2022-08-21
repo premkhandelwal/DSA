6c26d5eb4a13215fd9f30ac82caabc0d208ad515
Prem Khandelwal <prem.dkhandelwal@gmail.com>
2022-08-21 15:03:06 +0530

    Update ratInAMaze.cpp

--- Graph/ratInAMaze.cpp
+++ Graph/ratInAMaze.cpp
@@ -62,3 +62,80 @@ int main(){
 	return 0;
 }
 // } Driver Code Ends
+
+
+-------------------------------------------------------------------------------Working----------------------------------------------------------------
+	
+	//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+    public:
+    void dfsTraversal(vector<vector<int>> &m, int n, int i, int j, vector<string>& ans, string currStr, vector<vector<int>> &visited){
+        if(i < 0 || j < 0 || i > n -1 || j > n -1 || m[i][j] == 0 || visited[i][j] == 1){
+            return;
+        }
+        if(i == n -1 && j == n -1){
+            ans.push_back(currStr);
+            return;
+        }
+        visited[i][j] = 1;
+        dfsTraversal(m, n, i- 1, j, ans, currStr + 'U', visited);
+        dfsTraversal(m, n, i, j - 1, ans, currStr + 'L', visited);
+        dfsTraversal(m, n, i, j + 1, ans, currStr + 'R', visited);
+        dfsTraversal(m, n, i + 1, j, ans, currStr + 'D', visited);
+        visited[i][j] = 0;
+        
+        // currStr = "";
+        // return ans;
+    }
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        // Your code goes here
+        vector<string> ans;
+        vector<vector<int>> visited;
+        for(int i = 0; i < n; i++){
+            visited.push_back(vector<int>(n,0));
+        }
+        
+        string s = "";
+        dfsTraversal(m, n, 0, 0, ans, s, visited);
+        return ans;
+    }
+};
+
+    
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> m(n, vector<int> (n,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> m[i][j];
+            }
+        }
+        Solution obj;
+        vector<string> result = obj.findPath(m, n);
+        sort(result.begin(), result.end());
+        if (result.size() == 0)
+            cout << -1;
+        else
+            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
