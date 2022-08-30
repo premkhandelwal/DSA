c2c4da488ba63a45f4644524a1c1c92ad509cb5f
Prem Khandelwal <prem.dkhandelwal@gmail.com>
2022-08-30 20:00:55 +0530

    Update longestCommonSubSeq.cpp

--- Strings/longestCommonSubSeq.cpp
+++ Strings/longestCommonSubSeq.cpp
@@ -39,3 +39,67 @@ int lcs(string s, string t)
     vector<vector<int>> dp(n + 1 ,vector<int>(m + 1, -1));
     return f(n,m, s, t, dp);
 }
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    #include <vector>
+
+int lcs(string s, string t)
+{
+    int n = s.length();
+    int m = t.length();
+    vector<vector<int>> dp(n + 1 ,vector<int>(m + 1, -1));
+    for(int j = 0; j < m; j++){
+        dp[0][j] = 0;
+    }
+    for(int i = 0; i < n; i++){
+        dp[i][0] = 0;
+    }
+    
+    for(int ind1 = 1; ind1 <= n; ind1++){
+        for(int ind2 = 1; ind2 <= m; ind2++){
+             if(s[ind1 - 1] == t[ind2 - 1]){
+                 dp[ind1][ind2] =  1 + dp[ind1 - 1][ind2 - 1];    
+             }else{
+                   dp[ind1][ind2] = 0 + max(dp[ind1][ind2 - 1],dp[ind1 - 1][ind2]);   
+             }
+        
+        }
+    }
+    return dp[n][m];
+//     return f(n,m, s, t, dp);
+}
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------
+    
+    
+    #include <vector>
+
+int lcs(string s, string t)
+{
+    int n = s.length();
+    int m = t.length();
+    vector<int> prev(m+1,0), curr(m+1,0);
+//     vector<vector<int>> dp(n + 1 ,vector<int>(m + 1, -1));
+    for(int j = 0; j <= m; j++){
+        prev[j] = 0;
+    }
+    
+    
+    for(int ind1 = 1; ind1 <= n; ind1++){
+        for(int ind2 = 1; ind2 <= m; ind2++){
+             if(s[ind1 - 1] == t[ind2 - 1]){
+                 curr[ind2] =  1 + prev[ind2 - 1];    
+             }else{
+                   curr[ind2] = 0 + max(curr[ind2 - 1],prev[ind2]);   
+             }
+        
+        }
+        prev = curr;
+    }
+    return prev[m];
+//     return f(n,m, s, t, dp);
+}
