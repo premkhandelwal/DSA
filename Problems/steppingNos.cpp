6f00cb2aa40c524e7d39473fc1212d42a30e19f8
Prem Khandelwal <prem.dkhandelwal@gmail.com>
2022-10-23 23:12:00 +0530

    Update steppingNos.cpp

--- Problems/steppingNos.cpp
+++ Problems/steppingNos.cpp
@@ -65,3 +65,84 @@ int main()
 
 
 // } Driver Code Ends
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+public:
+
+    int bfs(int m)
+    {
+        int ans = 1;
+        queue<int> q;
+        for(int i = 1; i <= 9; i++){
+            q.push(i);    
+        }  
+        if (m < 0)
+            return 0;
+        if (m < 10)
+            return m + 1;
+        while(!q.empty()){
+            int num = q.front();
+            q.pop();
+            ans++;
+            int lastDigit = num % 10;
+            // 2 - 2*10 +  1
+            // 23 - 23*10 + 1
+            int num1 = (num * 10) + lastDigit + 1;
+            int num2 = (num * 10) + lastDigit - 1;
+            if (num % 10 == 0)
+            {
+                if (num1 <= m)
+                    q.push(num1);
+            }
+            // lastdigit is 9 then only one possible option that is append 8  that is  num2 here
+            else if (num % 10 == 9)
+            {
+                if (num2 <= m)
+                {
+                    q.push(num2);
+                }
+            }
+            // in all other case we will have 2 options append lastdigit+1 and lastDigit-1 at end of curr num
+            else
+            {
+                if (num1 <= m)
+                    q.push(num1);
+                if (num2 <= m)
+                    q.push(num2);
+            }
+        }
+        return ans;
+    }
+    
+    int steppingNumbers(int n, int m){
+        return bfs(m) - bfs(n-1);
+    }
+    
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    Solution obj;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        cin>>n>>m;
+        cout << obj.steppingNumbers(n,m) << endl;
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
